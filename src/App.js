767d27d5d96a1e3965bb289fbc5ed82fd7488f1f
Honz Williams <hnzwllms@gmail.com>
2022-04-11 16:21:36 -0400

    added getToken dependency, created onClick handler which creates token and handles state

--- src/App.js
+++ src/App.js
@@ -1,59 +1,56 @@
 import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import Firebase from './Pages/firebase';
-
-function List() {
-  return (
-    <>
-      <h1>List of Items</h1>
-    </>
-  );
-}
-
-function AddAnItem() {
-  return (
-    <>
-      <h1>Add an Item</h1>
-    </>
-  );
-}
-
-function NavLinks() {
-  return (
-    <nav style={{ position: 'fixed', bottom: 0 }}>
-      <NavLink
-        to="/"
-        style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold' } : { fontWeight: 'normal' }
-        }
-      >
-        List of Items
-      </NavLink>
-      <NavLink
-        to="/add-an-item"
-        style={({ isActive }) =>
-          isActive ? { fontWeight: 'bold' } : { fontWeight: 'normal' }
-        }
-      >
-        Add an Item
-      </NavLink>
-    </nav>
-  );
-}
+import { getToken } from '@the-collab-lab/shopping-list-utils';
+import { useState } from 'react';
+import AddItem from './Pages/AddItem';
+import NavLinks from './Navigation/NavLinks';
+import ItemList from './Pages/ItemList';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    localStorage.setItem('dummy', JSON.stringify({ token: getToken() }));
+    console.log(localStorage.dummy);
+
+    setToken(true);
+    navigate('/item-list');
+  };
+
+  const deleteStorage = () => {
+    localStorage.removeItem('dummy');
+
+    setToken(false);
+    navigate('/');
+  };
+  console.log(token);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Firebase />
-      </header>
-      <h1>Welcome to your Smart Shopping List!</h1>
+      <header className="App-header"></header>
+      <div>
+        {token === true ? (
+          <>
+            <button onClick={deleteStorage}>logout</button>
+            <NavLinks />
+          </>
+        ) : (
+          <>
+            <header className="header">
+              Welcome to your Smart Shopping List!
+            </header>
+
+            <button onClick={onClick}>create a new list</button>
+          </>
+        )}
+      </div>
       <Routes>
-        <Route path="/" element={<List />} />
-        <Route path="add-an-item" element={<AddAnItem />} />
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="item-list" element={<ItemList />} />
+        <Route path="add-item" element={<AddItem />} />
       </Routes>
-      <NavLinks />
     </div>
   );
 }
---
+++ src/Pages/ItemList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ItemList() {
+  return (
+    <>
+      <h1>List of Items</h1>
+    </>
+  );
+}
+
+export default ItemList;
