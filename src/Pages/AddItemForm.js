fa1228ba65abf1207823e8adbc3021760790b73f
jmc617 <jmc617@gmail.com>
2022-04-11 23:23:12 -0400

    additemform file created

---
+++ src/Pages/AddItemForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AddItemForm() {
+  return (
+    <>
+      //TODO: check accessibility requirements //Item name //purchase intervals
+      //user token //submit button
+      <form onSubmit={this.handleSubmit}></form>
+    </>
+  );
+}
+
+export default AddItemForm;
