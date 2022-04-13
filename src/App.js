357dee5d13808f4d20b52888d68d78feeac30758
Honz Williams <hnzwllms@gmail.com>
2022-04-13 17:23:34 -0400

    state saved to local with useeffect magic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import { getToken } from '@the-collab-lab/shopping-list-utils';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddItem from './Pages/AddItem';
 import NavLinks from './Navigation/NavLinks';
 import ItemList from './Pages/ItemList';
@@ -10,6 +10,19 @@ import ItemList from './Pages/ItemList';
 function App() {
   const [token, setToken] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const json = localStorage.getItem('dummy');
+    const loadedToken = JSON.parse(json);
+    if (loadedToken) {
+      setToken(loadedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(token);
+    localStorage.setItem('dummy', json);
+  }, [token]);
 
   const onClick = () => {
     localStorage.setItem('dummy', JSON.stringify({ token: getToken() }));
