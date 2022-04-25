ed242f92549b031dddb62cef20f2802e57f2bfcd
Honz Williams <hnzwllms@gmail.com>
2022-04-25 12:50:07 -0400

    setTimeout issues

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -4,6 +4,7 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 
 function ItemList({ token }) {
   const [docs, setDocs] = useState([]);
+  const [checkboxStatus, setCheckboxStatus] = useState(false);
 
   useEffect(() => {
     const tokenQuery = query(
@@ -25,12 +26,29 @@ function ItemList({ token }) {
     queryToken();
   }, [token]);
 
+  const checked = () => {
+    // let timestamp = Timestamp.now().toMillis();
+    // let date = new Date(timestamp);
+    //console.log(date)
+
+    let checkbox = document.getElementById('myCheckbox');
+
+    if (checkboxStatus === true) {
+      console.log('checked');
+    }
+  };
+
+  // setTimeout(checked, 2000)
+
   return (
     <>
       <h1>Your Items</h1>
       <h2>your token: {token}</h2>
       {docs.map((doc) => (
-        <p key={doc.id}>{doc.item_name}</p>
+        <>
+          <input id="myCheckbox" type="checkbox" onClick={checked} />
+          <p key={doc.id}>{doc.item_name}</p>
+        </>
       ))}
     </>
   );
