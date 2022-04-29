8edaab5a089f1b16ba4daa77c3a08b45d8656918
Estefani <stef45689@gmail.com>
2022-04-29 17:04:53 -0500

    rework the date to db

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -3,9 +3,12 @@ import { useEffect, useState } from 'react';
 import {
   collection,
   getDocs,
+  updateDoc,
+  doc,
   query,
   where,
   Timestamp,
+  serverTimestamp,
 } from 'firebase/firestore';
 
 function ItemList({ token }) {
@@ -32,26 +35,33 @@ function ItemList({ token }) {
     queryToken();
   }, [token]);
 
-  const isClicked = (item_name) => {
-    console.log(item_name);
+  //we need to save the session so the box unchecks only after the time is over. regardless of refresh or logging out
+
+  //click & save, retrieve
+  //is this date > or < Timestamp.now() + 24hrs
+
+  const isClicked = (id) => {
+    const newDoc = doc(db, 'groceries', id);
+    updateDoc(newDoc, { last_purchased_date: Date.now() });
+    console.log(id);
 
     const one = 5000;
     const thisTimestamp = Timestamp.now().toDate();
     // const theCheckbox = document.getElementById('myCheckbox');
 
-    setTimeout(() => {
-      docs.map((doc) => {
-        const theCheckbox = document.getElementById(doc.item_name);
+    //   setTimeout(() => {
+    //     docs.map((doc) => {
+    //       const theCheckbox = document.getElementById(doc.item_name);
 
-        if (theCheckbox.checked === true) {
-          if (doc.item_name === item_name) {
-            doc.last_purchased_date = thisTimestamp;
-            console.log(doc.item_name, doc.last_purchased_date);
-            theCheckbox.checked = false;
-          }
-        }
-      });
-    }, one);
+    //       if (theCheckbox.checked === true) {
+    //         if (doc.item_name === item_name) {
+    //           doc.last_purchased_date = thisTimestamp;
+    //           console.log(doc.item_name, doc.last_purchased_date);
+    //           theCheckbox.checked = false;
+    //         }
+    //       }
+    //     });
+    //   }, one);
   };
 
   return (
@@ -59,12 +69,16 @@ function ItemList({ token }) {
       <h1>Your Items</h1>
       <h2>your token: {token}</h2>
       {docs.map((doc) => {
+        if (doc.last_purchased_date) {
+          console.log(doc.last_purchased_date, Date.now());
+        }
         return (
           <div>
             <input
-              id={doc.item_name}
+              checked={Date.now() - doc.last_purchased_date < 5000}
+              id={doc.id}
               type="checkbox"
-              onChange={() => isClicked(doc.item_name)}
+              onChange={() => isClicked(doc.id)}
             />
             <p key={doc.id}>{doc.item_name}</p>
           </div>
